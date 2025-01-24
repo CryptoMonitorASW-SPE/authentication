5b18f17958605f14e428d2b8222d4cd2ca436920
Alessandro Becci <trofui@gmail.com>
2025-01-24 17:01:37 +0100

    fix: fix sign method of jwt after update

--- app/src/infrastructure/adapters/JwtTokenService.ts
+++ app/src/infrastructure/adapters/JwtTokenService.ts
@@ -1,14 +1,15 @@
-import jwt from 'jsonwebtoken'
+import jwt, { SignOptions } from 'jsonwebtoken'
 import { TokenService } from '../../domain/ports/TokenService'
 
 export class JwtTokenService implements TokenService {
   constructor(
     private readonly secret: string,
-    private readonly expiration = '1h'
+    private readonly expiration: string | number = '1h'
   ) {}
 
   generateToken(userId: string, email: string): string {
-    return jwt.sign({ userId, email }, this.secret, { expiresIn: this.expiration })
+    var token = jwt.sign({ userId, email }, this.secret)
+    return token
   }
 
   verifyToken(token: string): { userId: string; email: string } {
